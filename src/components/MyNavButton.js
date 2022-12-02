2b7cf22fbf913c3641c1969235f077d52add4b01
Bob Dain <bob.dain@se.com>
2022-12-02 00:05:51 -0800

    spring on navButton is working

--- src/components/MyNavButton.js
+++ src/components/MyNavButton.js
@@ -1,21 +1,55 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { store } from '../store.js';
+import { useSpring, animated } from 'react-spring'
 
 const MyNavButton = (props) => {
-    const style = { 
-        fontSize: '100%', 
-        transition: 'font-size 0.2s'}
+    const style = {
+        fontSize: '100%',
+        transition: 'font-size 0.2s'
+    }
+
+    const onClick = (buttonId) => {
+        console.log(`Button clciked: ${props.buttonId}`);
+        setStep({
+            fontSizeStart: '100%',
+            fontSizeEnd: '100%',
+        });
+    }
+
+    const onMouseEnter = (buttonId) => {
+        console.log(`Mouse enter: ${props.buttonId}`);
+        setStep({
+            fontSizeStart: '100%',
+            fontSizeEnd: '120%',
+        });
+    }
+
+    const onMouseLeave = (buttonId) => {
+        console.log(`Mouse leave: ${props.buttonId}`);
+        setStep({
+            fontSizeStart: '120%',
+            fontSizeEnd: '100%',
+        });
+    }
+
+    const [step, setStep] = useState({
+        fontSizeStart: '100%',
+        fontSizeEnd: '100%',
+    });
+
+    const spring = useSpring({
+        fontSize: step.fontSizeEnd,
+        from: step.fontSizeStart
+    });
 
     return (
-        <button className={props.className} style={style} 
-            onMouseEnter={({ target }) => target.style.fontSize = "120%"}
-            onMouseLeave={({ target }) => target.style.fontSize = "100%"} 
-            onClick={({ target }) => {
-                console.log(`Pressed button #${props.buttonId}`)
-            }}>
-                {props.text}
-            </button>
-    )
+        <animated.button className={props.className} style={spring}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onClick={onClick}>
+            {props.text}
+        </animated.button>
+    );
 }
 
 export default MyNavButton;
